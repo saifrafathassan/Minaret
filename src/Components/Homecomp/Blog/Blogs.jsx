314879427ca16871bb08f7102dcd6b7f85067602
@hkaram <hkaram@devsolutions.team>
2024-08-27 11:58:51 +0300

    fixing blog section hover and spaces

--- src/Components/Homecomp/Blog/Blogs.jsx
+++ src/Components/Homecomp/Blog/Blogs.jsx
@@ -12,12 +12,12 @@ class Blogs extends Component {
         const bolgs=data['bolgs']
 
         return (
-            <section className="padc bolg-container" >
-                <div className="title" >
+            <section className="padc bolg-container py-[100px]" >
+                <div className="title text-[50px]" >
                    Latest From <span>The Blog</span>
                 </div>
 
-                <div className="desc width-50">
+                <div className="desc width-50 pt-[20px] pb-[30px]">
                 Stay updated with the latest insights in digital marketing.
                  Our blog is a treasure trove of knowledge and tips for your brand's journey.
                 </div>
@@ -25,12 +25,12 @@ class Blogs extends Component {
                 <div className="blog-card-container">
                         {bolgs.map((blog, index) => (
                             <div key={index} className="blog">
-                                <img src={blog.image} alt={`Blog ${index + 1}`}  />
+                                <img className="bg-img" src={blog.image} alt={`Blog ${index + 1}`}  />
                                 <div className="bolg-title "> {blog.title} </div>
-                                <div className="blog-content" >{blog.content}</div>
-                                <div className='read-more '>
-                                    <div className='t'> <p>Read More</p></div>
-                                    <div className='m'> <img src={arow} alt="" /></div>
+                                <div className="blog-content py-[15px]" >{blog.content}</div>
+                                <div className='read-more'>
+                                     Read More
+                                    <img className="pl-[10px]" src={arow} alt="" />
                                 </div>
                             </div>
                         ))}
