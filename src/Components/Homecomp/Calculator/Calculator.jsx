03a0e4af69c94382789d899e60594f63e918071c
Saif <saifrafaat99@gmail.com>
2024-10-07 07:54:57 +0300

    letters for en language & Calculator

--- src/Components/Homecomp/Calculator/Calculator.jsx
+++ src/Components/Homecomp/Calculator/Calculator.jsx
@@ -41,31 +41,6 @@ const Calculator = ({t}) => {
             <div className='calculator-text'>
                 {t("Calculate")}
             </div>
-            <div className="calculator">
-                <input type="text" value={input} readOnly className="calculator-display" />
-                <div className="calculator-buttons">
-                    <button className='bg-slate-500' onClick={() => handleClick("1")}>1</button>
-                    <button className='bg-slate-500' onClick={() => handleClick("2")}>2</button>
-                    <button className='bg-slate-500' onClick={() => handleClick("3")}>3</button>
-                    <button className='bg-slate-800' onClick={() => handleClick("+")}>+</button>
-                    <button className='bg-slate-500' onClick={() => handleClick("4")}>4</button>
-                    <button className='bg-slate-500' onClick={() => handleClick("5")}>5</button>
-                    <button className='bg-slate-500' onClick={() => handleClick("6")}>6</button>
-                    <button className='bg-slate-800' onClick={() => handleClick("-")}>-</button>
-                    <button className='bg-slate-500' onClick={() => handleClick("7")}>7</button>
-                    <button className='bg-slate-500' onClick={() => handleClick("8")}>8</button>
-                    <button className='bg-slate-500' onClick={() => handleClick("9")}>9</button>
-                    <button className='bg-slate-800' onClick={() => handleClick("*")}>x</button>
-                    <button className='bg-slate-500' onClick={() => handleClick("0")}>0</button>
-                    <button className='bg-slate-500' onClick={() => handleClick(".")}>.</button>
-                    <button className='bg-slate-500' onClick={handleBackspace}>⌫</button>
-                    <button className='bg-blue-800' onClick={handleCalculate}>=</button>
-                    <button className='bg-red-500' onClick={handleClear}>AC</button>
-                    <button className='bg-slate-800' onClick={() => handleClick("/")}>/</button>
-                    <button className='bg-slate-800' onClick={() => handleClick("^")}>^</button> 
-                    <button className='bg-slate-800' onClick={() => handleClick("%")}>%</button>
-                </div>
-            </div>
         </div>
     );
 }
