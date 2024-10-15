c311e47c5c39f8b25898856563da7832fd103a8c
Saif <saifrafaat99@gmail.com>
2024-10-15 23:28:54 +0300

    team

--- src/Pages/AboutPage/team.jsx
+++ src/Pages/AboutPage/team.jsx
@@ -3,12 +3,14 @@ import { Component } from "react";
 import { TiSocialFacebook } from "react-icons/ti";
 import { TiSocialLinkedin } from "react-icons/ti";
 import { FiPhone } from "react-icons/fi";
-import soft from '/Assets/images/AboutPage/team/2.png'
+import Mohamidali from '/Assets/images/AboutPage/team/2.png'
 import ceo from '/Assets/images/AboutPage/team/3.png'
-import Grahic from '/Assets/images/AboutPage/team/4.png'
-import Webdev from '/Assets/images/AboutPage/team/5.png'
-import Teammanger from '/Assets/images/AboutPage/team/6.png'
-import SrDisgner from '/Assets/images/AboutPage/team/7.png'
+import Amira from '/Assets/images/AboutPage/team/4.png'
+import Abdelrhman from '/Assets/images/AboutPage/team/5.png'
+import Gehad from '/Assets/images/AboutPage/team/6.png'
+import Shimaa from '/Assets/images/AboutPage/team/7.png'
+import saif from '/Assets/images/AboutPage/team/8.png'
+import Doaa from '/Assets/images/AboutPage/team/9.png'
 import { withTranslation } from 'react-i18next';
 
 class Team extends Component {
@@ -59,7 +61,7 @@ class Team extends Component {
 
                     <div className="member">
                         <div className="mem-image">
-                            <img src={soft} alt="" />
+                            <img src={Mohamidali} alt="" />
                         </div>
                         <div className="mem-data ">
                             <div className="Name ">
@@ -88,7 +90,7 @@ class Team extends Component {
 
                     <div className="member">
                         <div className="mem-image">
-                            <img src={Webdev} alt="" />
+                            <img src={Abdelrhman} alt="" />
                         </div>
                         <div className="mem-data ">
                             <div className="Name ">
@@ -117,7 +119,7 @@ class Team extends Component {
 
                     <div className="member">
                         <div className="mem-image">
-                            <img src={Teammanger} alt="" />
+                            <img src={Gehad} alt="" />
                         </div>
                         <div className="mem-data ">
                             <div className="Name ">
@@ -146,7 +148,7 @@ class Team extends Component {
 
                     <div className="member">
                         <div className="mem-image">
-                            <img src={SrDisgner} alt="" />
+                            <img src={Shimaa} alt="" />
                         </div>
                         <div className="mem-data ">
                             <div className="Name ">
@@ -175,7 +177,7 @@ class Team extends Component {
 
                     <div className="member h-full">
                         <div className="mem-image">
-                            <img src={Grahic} alt="" />
+                            <img src={Amira} alt="" />
                         </div>
                         <div className="mem-data ">
                             <div className="Name ">
@@ -187,6 +189,64 @@ class Team extends Component {
                             <hr/>
                             <div className="summary lg:h-[190px]" >
                                 <p>{t("Amira")}</p>
+                            </div>
+                            <div className="social">
+                                <div className="icon"> <TiSocialLinkedin size={20} /></div>
+                                <div className="icon"> <TiSocialFacebook size={20}/></div>
+                                <div className="icon"> <FiPhone size={20}/></div>
+
+                            </div>
+                            <div className="social2">
+                                <div className="icon"> <TiSocialLinkedin size={10} /></div>
+                                <div className="icon"> <TiSocialFacebook size={10}/></div>
+                                <div className="icon"> <FiPhone size={10}/></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="member h-full">
+                        <div className="mem-image">
+                            <img src={saif} alt="" />
+                        </div>
+                        <div className="mem-data ">
+                            <div className="Name ">
+                                <p>{t("Saif R Mostafa")}  </p>
+                            </div>
+                            <div className="job">
+                            {t("Front-End Developer")}
+                            </div>
+                            <hr/>
+                            <div className="summary lg:h-[180px]" >
+                                <p>{t("Saif")}</p>
+                            </div>
+                            <div className="social">
+                                <div className="icon"> <TiSocialLinkedin size={20} /></div>
+                                <div className="icon"> <TiSocialFacebook size={20}/></div>
+                                <div className="icon"> <FiPhone size={20}/></div>
+
+                            </div>
+                            <div className="social2">
+                                <div className="icon"> <TiSocialLinkedin size={10} /></div>
+                                <div className="icon"> <TiSocialFacebook size={10}/></div>
+                                <div className="icon"> <FiPhone size={10}/></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="member h-full">
+                        <div className="mem-image">
+                            <img src={Doaa} alt="" />
+                        </div>
+                        <div className="mem-data ">
+                            <div className="Name ">
+                                <p>{t("Doaa Asqalany")}  </p>
+                            </div>
+                            <div className="job">
+                            {t("Account Manager")}
+                            </div>
+                            <hr/>
+                            <div className="summary lg:h-[190px]" >
+                                <p>{t("Doaa")}</p>
                             </div>
                             <div className="social">
                                 <div className="icon"> <TiSocialLinkedin size={20} /></div>
