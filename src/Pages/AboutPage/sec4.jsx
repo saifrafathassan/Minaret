e19288456054b7a1899d5fec0d4d46bf66c10b27
Saif <saifrafaat99@gmail.com>
2024-11-07 09:46:33 +0200

    about styling

--- src/Pages/AboutPage/sec4.jsx
+++ src/Pages/AboutPage/sec4.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Component } from "react";
 import image from '/Assets/images/AboutPage/4.png'
 import './about.css';
+import { motion } from 'framer-motion';
 import { Padding } from "@mui/icons-material";
 import { withTranslation } from 'react-i18next';
 import i18n from 'i18next';
@@ -11,9 +12,27 @@ class Sec4 extends Component {
     render() { 
         const { t } = this.props;
         const currentLanguage = i18n.language.split('-')[0];
+
+        const textVariants = {
+            hidden: { opacity: 0, y: -50 },
+            visible: { opacity: 1, y: 0 },
+        };
+
+        const imageVariants = {
+            hidden: { opacity: 0, x: 50 },
+            visible: { opacity: 1, x: 0 },
+        };
+
         return (
             <section className="sec2 padc bg1 mission">
                     <div className="left-about " >
+                        <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        variants={textVariants}
+                        transition={{ duration: 1 }}
+                        viewport={{ once: true, amount: 0.5 }}
+                        >
                        <div>
                        <div className="title missionTitle"  style={{marginBottom:'10px'}}>
                         {/* {t("Our")} <span>{t("Mession")}</span> */}
@@ -23,6 +42,7 @@ class Sec4 extends Component {
                             <p>
                             {t("Dedicated")}</p>
                         </div>
+
                        </div>
                        <div>
                        <div className="title" style={{marginTop: '20px',marginBottom:'10px'}}>
@@ -34,14 +54,22 @@ class Sec4 extends Component {
                             {t("We envision")}</p>
                         </div>
                        </div>
-
+                       </motion.div>
                     </div>
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        variants={imageVariants}
+                        transition={{ duration: 1 }}
+                        viewport={{ once: true, amount: 0.5 }}
+                        >
                     <div className="right">
                         <div className="imagepana">
                         <img src={image} alt="" />
                         </div>
                         
                     </div>
+                    </motion.div>
             </section>
         );
     }
